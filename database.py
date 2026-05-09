8f89f2f0a4d4c31aed13445f2c294e3caf5b8f4a
Phoenix-Dong02 <a1912655@adelaide.edu.au>
2026-05-09 20:20:01 +0930

    secure supabase with service role key

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from supabase import create_client
 @st.cache_resource
 def get_supabase_client():
     url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
+    key = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
     return create_client(url, key)
 
 
